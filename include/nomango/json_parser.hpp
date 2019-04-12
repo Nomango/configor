8f391acfd57b0883877b814f040b334581371106
Nomango <569629550@qq.com>
2019-04-12 18:15:10 +0800

    minor

--- include/nomango/json_parser.hpp
+++ include/nomango/json_parser.hpp
@@ -1,4 +1,4 @@
-// Copyright (c) 2016-2018 Kiwano - Nomango
+// Copyright (c) 2016-2018 JSON-cpp - Nomango
 // 
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
